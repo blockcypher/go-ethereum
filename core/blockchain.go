56ed869eb770dd7c2d44d4b8913a009836f9c968
Matthieu Riou <matthieu@offthelip.org>
2022-03-16 01:57:16 -0700

    Trying to remove state validation temporarily

--- core/blockchain.go
+++ core/blockchain.go
@@ -1613,12 +1613,12 @@ func (bc *BlockChain) insertChain(chain types.Blocks, verifySeals, setHead bool)
 		blockExecutionTimer.Update(time.Since(substart) - trieproc - triehash)
 
 		// Validate the state using the default validator
-		substart = time.Now()
-		if err := bc.validator.ValidateState(block, statedb, receipts, usedGas); err != nil {
-			bc.reportBlock(block, receipts, err)
-			atomic.StoreUint32(&followupInterrupt, 1)
-			return it.index, err
-		}
+		// substart = time.Now()
+		// if err := bc.validator.ValidateState(block, statedb, receipts, usedGas); err != nil {
+		// 	bc.reportBlock(block, receipts, err)
+		// 	atomic.StoreUint32(&followupInterrupt, 1)
+		// 	return it.index, err
+		// }
 		proctime := time.Since(start)
 
 		// Update the metrics touched during block validation
