70f7b3587942e76dcbf43d07b5a9be734ff9974d
Quentin Le Sceller <q.lesceller@gmail.com>
2023-01-25 16:17:28 -0500

    Increase websocket read limit to 50mb

--- rpc/websocket.go
+++ rpc/websocket.go
@@ -27,8 +27,8 @@ import (
 	"sync"
 	"time"
 
-	mapset "github.com/deckarep/golang-set"
 	"github.com/blockcypher/go-ethereum/log"
+	mapset "github.com/deckarep/golang-set"
 	"github.com/gorilla/websocket"
 )
 
@@ -38,7 +38,7 @@ const (
 	wsPingInterval     = 60 * time.Second
 	wsPingWriteTimeout = 5 * time.Second
 	wsPongTimeout      = 30 * time.Second
-	wsMessageSizeLimit = 15 * 1024 * 1024
+	wsMessageSizeLimit = 50 * 1024 * 1024
 )
 
 var wsBufferPool = new(sync.Pool)
